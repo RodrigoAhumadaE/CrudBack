9fcad508da0da926cc25f271bcbc96433b81c75a
RodAhumada <rod.ahumada@duocuc.cl>
2023-11-22 15:01:48 -0300

    correcciones finales

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CrudBack.Models;
+using System.Net;
 
 namespace CrudBack.Controllers;
 
@@ -13,12 +14,21 @@ public class APIController : Controller{
         _context = context;
     }
 
-    // GET
     [HttpGet("api/usuarios")]
-    public JsonResult APIUsuarios(){
-        // List<Usuario> ListaUsuarios = _context.Usuarios.ToList();
-        List<Usuario> ListaUsuarios = _context.Usuarios.ToList();
-        return Json(ListaUsuarios);
+    public JsonResult APIUsuarios([FromQuery(Name = "rut")] string rutParam){
+        List<Usuario> listaUsuarios;
+
+        if (string.IsNullOrEmpty(rutParam)){
+            listaUsuarios = _context.Usuarios.ToList();
+        }
+        else{
+            listaUsuarios = _context.Usuarios.Where(u => u.Rut.Contains(rutParam)).ToList();
+            if(listaUsuarios.Count == 0){
+                listaUsuarios = _context.Usuarios.ToList();
+                return Json(listaUsuarios, HttpStatusCode.NotFound);
+            }
+        }
+        return Json(listaUsuarios);
     }
 
     [HttpGet("api/usuario/{id}")]
@@ -43,6 +53,46 @@ public class APIController : Controller{
 
         return Ok();
     }
+    
+    // PUT
+    [HttpPut("api/usuario/actualizar")]
+    public async Task<ActionResult> ActualizarUsuario([FromBody] Usuario usuario){
+        if (usuario == null){
+            return BadRequest();
+        }
+
+        var existingUser = await _context.Usuarios.FindAsync(usuario.UsuarioId);
+        if (existingUser == null){
+            return NotFound();
+        }
+
+        existingUser.Rut = usuario.Rut;
+        existingUser.Nombre = usuario.Nombre;
+        existingUser.Apellido = usuario.Apellido;
+        existingUser.Correo = usuario.Correo;
+        existingUser.Telefono = usuario.Telefono;
+
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    // DELETE
+    [HttpDelete("api/usuario/eliminar/{id}")]
+    public async Task<ActionResult> EliminarUsuario(int id){
+        var existingUser = await _context.Usuarios.FindAsync(id);
+
+        if (existingUser == null){
+            return NotFound();
+        }
+
+        _context.Usuarios.Remove(existingUser);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error(){
